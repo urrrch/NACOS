e354a5777fe7536cfa2953c4fabab208b2f46a3b
wizardchiboy <doncodesacademy@gmail.com>
2022-09-15 10:51:51 +0100

    fix: logo on small screen

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+//   dynamic logo change
+const logo = document.getElementById("logo");
+
+if (screen.width < 900) {
+  logo.src = "./img/NACOSS site.png";
+} else {
+  window.onscroll = function scroll() {
+    window.scrollY > 45
+      ? (logo.src = "./img/NACOSS site.png")
+      : (logo.src = "./img/footer_nacos.png");
+  };
+
+  scroll();
+}
+
 (function ($) {
   "use strict";
 
@@ -22,17 +37,6 @@
       $(".navbar").removeClass("sticky-top shadow-sm");
     }
   });
-
-  //   dynamic logo change
-  const logo = document.getElementById("logo");
-
-  window.onscroll = function scroll() {
-    window.scrollY > 45
-      ? (logo.src = "./img/NACOSS site.png")
-      : (logo.src = "./img/footer_nacos.png");
-  };
-
-  scroll();
 
   // Dropdown on mouse hover
   const $dropdown = $(".dropdown");
